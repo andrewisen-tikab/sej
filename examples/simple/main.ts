38489035ec133dcf0f7a8b51936f81b5c42206fd
André "MG" Wisén <andre.wisen@gmail.com>
2023-05-23 20:59:03 +0200

    Add `loading` events

--- examples/simple/main.ts
+++ examples/simple/main.ts
@@ -10,5 +10,33 @@ Sej.addEventListener(SejEventKeys.init, (e) => {
     console.log('Init done!');
 });
 
+Sej.addEventListener(SejEventKeys.onStart, (e) => {
+    const {
+        data: { url, itemsLoaded, itemsTotal },
+    } = e;
+    console.log(
+        'Started loading file: ' +
+            url +
+            '.\nLoaded ' +
+            itemsLoaded +
+            ' of ' +
+            itemsTotal +
+            ' files.',
+    );
+});
+
+Sej.addEventListener(SejEventKeys.onProgress, (e) => {
+    const {
+        data: { url, itemsLoaded, itemsTotal },
+    } = e;
+    console.log(
+        'Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.',
+    );
+});
+
+Sej.addEventListener(SejEventKeys.onLoad, (e) => {
+    console.log('Loading complete!');
+});
+
 Sej.install();
 Sej.init({ container });
--- src/core/events/types.ts
+++ src/core/events/types.ts
@@ -3,10 +3,23 @@ export const SejEventKeys = {
      * Runs when Sej is initialized.
      */
     init: 'init',
+    onStart: 'onStart',
+    onLoad: 'onLoad',
+    onProgress: 'onProgress',
+    onError: 'onError',
 } as const;
 
+export type Loading = {
+    url: string;
+    itemsLoaded: number;
+    itemsTotal: number;
+};
 export type SejEventsBase = {
     [SejEventKeys.init]: { type: typeof SejEventKeys.init; data?: undefined };
+    [SejEventKeys.onStart]: { type: typeof SejEventKeys.onStart; data: Loading };
+    [SejEventKeys.onLoad]: { type: typeof SejEventKeys.onLoad; data?: undefined };
+    [SejEventKeys.onProgress]: { type: typeof SejEventKeys.onProgress; data: Loading };
+    [SejEventKeys.onError]: { type: typeof SejEventKeys.onError; data: { url: string } };
 };
 
 export interface SejEvents extends SejEventsBase {}
