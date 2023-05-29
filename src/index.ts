3802b0370d1f860e8ae1b40c42738c181b8e61de
André "MG" Wisén <andre.wisen@gmail.com>
2023-05-29 12:50:20 +0200

    Add `SejEvents`

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { default as _ } from './Sej';
-
+import type { SejEvents } from './core/events/types';
 /**
  * Sej Engine as singleton.
  */
 const Sej = _.Instance;
 
 export default Sej;
+export { SejEvents };
