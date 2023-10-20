cab468270c164b34cb04177b71661aed679015ab
André "MG" Wisén <andre.wisen@gmail.com>
2023-10-20 15:22:54 +0200

    nuke

--- examples/simple/main.ts
+++
@@ -1,7 +0,0 @@
-import '../assets/css/styles.css';
-
-// @ts-ignore
-import glb from '../assets/glb/spartan_armour_mkv_-_halo_reach.glb?url';
-import ExampleBuilder from '../ExampleBuilder';
-
-new ExampleBuilder({ url: glb, type: 'glb', up: '+Y' });
--- src/types.ts
+++
@@ -1,7 +0,0 @@
-export type InitProps = {
-    container: HTMLDivElement;
-    webGLFallback?: boolean;
-    useWebGL?: boolean;
-};
-
-export type Up = '+Y' | '+Z' | '-Z';
