83413cae7c4bf6345b436b9ec94540fdd3acf20e
André "MG" Wisén <andre.wisen@gmail.com>
2023-11-02 09:23:07 +0100

    Update `example` page

--- examples/examples.ts
+++ examples/examples.ts
@@ -1,3 +1,4 @@
+/* eslint-disable require-jsdoc */
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { EXAMPLES } from '../examples.js';
@@ -8,18 +9,55 @@ const h1 = document.createElement('h1');
 h1.textContent = 'Sej Examples';
 div.appendChild(h1);
 
-const ul = document.createElement('ul');
-ul.id = 'ul';
+const examples: { [key: string]: string[] } = {};
+const uncategorized: string[] = [];
 
-EXAMPLES.forEach((example: string) => {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    a.textContent = `${example}`;
-    a.href = `./examples/${example}/`;
-    li.appendChild(a);
-    ul.appendChild(li);
+EXAMPLES.sort().forEach((example: string) => {
+    // Find first slash
+    const slashIndex = example.indexOf('/');
+    // If found, grab the first part of the string
+    const category = slashIndex !== -1 ? example.substring(0, slashIndex) : null;
+
+    if (category) {
+        // Check if key exists
+        if (examples[category]) {
+            // If it does, push example to array
+            examples[category].push(example);
+        } else {
+            // If it doesn't, create new array with example
+            examples[category] = [example];
+        }
+    } else {
+        uncategorized.push(example);
+    }
 });
 
-div.appendChild(ul);
+const capitalizeFirstLetter = (string: string) => string.charAt(0).toUpperCase() + string.slice(1);
+
+const buildList = (list: string[], category: string) => {
+    const ul = document.createElement('ul');
+    ul.id = 'ul';
+
+    const h3 = document.createElement('h3');
+    h3.textContent = `${capitalizeFirstLetter(category)}`;
+    div.appendChild(h3);
+
+    list.forEach((example: string) => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.textContent = `${example}`;
+        a.href = `./examples/${example}/`;
+        li.appendChild(a);
+        ul.appendChild(li);
+    });
+
+    div.appendChild(ul);
+};
+
+buildList(uncategorized, 'Uncategorized');
+
+Object.keys(examples).forEach((category: string) => {
+    buildList(examples[category], category);
+});
 
 document.body.appendChild(div);
