99a81178f565528d009a5876331ccc395feb1d30
André Wisén <andre.wisen@tikab.com>
2025-01-05 23:07:11 +0100

    refactor AbstractSelector: replace error throw with history logging for object selection

--- src/selector/AbstractSelector.ts
+++ src/selector/AbstractSelector.ts
@@ -77,7 +77,12 @@ export class AbstractSelector implements Selector {
                 this.editor.signals.objectDeselected.dispatch(object);
             } else {
                 // If the object is not in the this.editor.selected array, throw an error
-                throw new Error('The object is not selected.');
+                ErrorManager.addHistory(
+                    new HistoryObject(
+                        Errors.SELECTOR_OBJECT_NOT_FOUND.key,
+                        Errors.SELECTOR_OBJECT_NOT_FOUND.message(object.uuid),
+                    ),
+                );
             }
         }
     }
