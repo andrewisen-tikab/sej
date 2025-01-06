3f381b8f5c465cb09d9c2536bb89689e4a7ab5f9
André Wisén <andre.wisen@tikab.com>
2025-01-06 08:32:47 +0100

    refactor SnakeKeyboardControls and AbstractEditor: adjust speed calculation and integrate spatial hash grid

--- src/editor/AbstractEditor.ts
+++ src/editor/AbstractEditor.ts
@@ -14,6 +14,8 @@ import type { History } from '../history/types';
 import { AbstractLoaderManager } from '../loader/AbstractLoaderManager';
 import type { LoaderManager } from '../loader/types';
 import { AbstractSelector } from '../selector/AbstractSelector';
+import { AbstractSpatialHashGrid } from '../spatial/AbstractSpatialHashGrid';
+import type { SpatialHashGrid } from '../spatial/types';
 import { MobileUtils } from '../utils/MobileUtils';
 import { Config } from './Config';
 import type { Editor, EditorJSON, EditorSignals, Object3D, Storage } from './types';
@@ -98,6 +100,8 @@ export abstract class AbstractEditor implements Editor {
 
     public selected: Object3D[];
 
+    public spatialHashGrid: SpatialHashGrid;
+
     public gisHelper: GISHelper;
 
     public history: History;
@@ -134,6 +138,9 @@ export abstract class AbstractEditor implements Editor {
 
         this.selector = new AbstractSelector(this);
         this.selected = [];
+
+        this.spatialHashGrid = new AbstractSpatialHashGrid();
+        this.scene.add(this.spatialHashGrid as AbstractSpatialHashGrid);
 
         this.gisHelper = new AbstractGISHelper();
 
